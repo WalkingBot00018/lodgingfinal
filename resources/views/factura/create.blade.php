a886a2126fe28cd226d1b718fb5a77ecbd9501b8
Aprendiz <Aprendiz@SOAAPRCIDFS313.FormacionSena.RED>
2024-03-12 17:28:57 -0500

    df

--- resources/views/factura/create.blade.php
+++ resources/views/factura/create.blade.php
@@ -1,5 +1,19 @@
-<a href="{{ url('factura')}}">regresar</a>
-<form action="{{ url('/factura')}}" method="post">
-    @csrf
-    @include('factura.form',['modo'=>'Crear'])
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/estilos.css">
+    <title>Factura</title>
+</head>
+<body>
+    <header>
+        <a href="{{ url('factura')}}">regresar</a>
+    </header>
+        <form action="{{ url('/factura')}}" method="post">
+        @csrf
+        @include('factura.form',['modo'=>'Crear'])
+        </form>
+    </body>
+</html>
+
