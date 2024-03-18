064df4c678add2f988ed3656bec7b57e6a55b86d
Aprendiz <Aprendiz@SOAAPRCIDFS313.FormacionSena.RED>
2024-03-18 13:24:35 -0500

    estilos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,74 +1,75 @@
 <body>
+    <a class="regresar" href="{{url('usuarios')}}">Regresar</a>
+    <section class="contenidoC">
+        <h1>Usuarios</h1>
+        <article>
+            <link rel="stylesheet" href="{{ asset('css/register.css') }}">
+            <form action="{{ route('user.store') }}" method="post">
+                @csrf
 
-    <a class="regresar" href="{{ asset('/') }}" >Regresar</a>
-   <form action="{{ route('user.store') }}" method="post">
-   <link rel="stylesheet" href="{{ asset('css/register.css') }}">
-    @csrf
-    
-    @if (session('mensaje'))
-        <h6>{{ session('') }}</h6>   
-    @endif
+                @if (session('mensaje'))
+                    <h6>{{ session('mensaje') }}</h6>
+                @endif
 
-    <label for="Nro_doc">Numero de Documento</label>
-    <input type="text" name="Nro_doc" id="" placeholder="Document Number" value="{{ old('Nro_doc') }}">
-    @error('Nro_doc')
-        <h6>{{ $message }}</h6>
-    @enderror
-    <label for="Nombre">Nombre</label>
-    <input type="text" name="Nombre" id="" placeholder="Nombre" value="{{ old('Nombre') }}">
-    @error('Nombre')
-        <h6>{{ $message }}</h6>
-    @enderror
-    <label for="Apellido">apellido</label>
-    <input type="text" name="Apellido" id="" placeholder="Apellido" value="{{ old('Apellido') }}">
-    @error('Apellido')
-        <h6>{{ $message }}</h6>
-    @enderror
-    <label for="Email">Email</label>
-    <input type="email" name="email" id="" placeholder="Email" value="{{ old('email') }}">
-    @error('email')
-        <h6>{{ $message }}</h6>
-    @enderror
-    <label for="Password">Password</label>
-    <input type="password" name="password" id="" placeholder="Password" value="{{ old('password') }}">
 
-    @error('password')
-        <h6>{{ $message }}</h6>
-    @enderror
-    <label for="Telefono">Telefono</label>
-    <input type="text" name="Telefono" id="" placeholder="telefono" value="{{ old('Telefono') }}">
-    @error('Telefono')
-        <h6>{{ $message }}</h6>
-    @enderror
+                <input class="contenedorinput" type="text" name="Nro_doc" id="" placeholder="Numero de documento" value="{{ old('Nro_doc') }}">
+                @error('Nro_doc')
+                    <h6>{{ $message }}</h6>
+                @enderror
 
-    <label for="Estado">Estado</label>
-    <select name="Estado" id="Estado">
-        <option value="Activo">Activo</option>
-        <option value="Inactivo">Inactivo</option>
-    </select>
-    @error('Estado')
-        <h6>{{ $message }}</h6>
-    @enderror
-    <label for="Rol">Rol</label>
-    <select name="ID_rol" id="ID_rol">
-        <option value="1">ADMINISTRADOR</option>
-        <option value="2">CLIENTE</option>
-        <option value="3">RECEPSIONISTA</option>
-        <option value="4">MANTENIMIENTO SISTEMA</option>
-        <option value="5">PERSONAL DE LIMPIEZA</option>
-        <option value="6">SEGURIDAD</option>
-    </select>
-    
-    
-    @error('ID_rol')
-        <h6>{{ $message }}</h6>
-    @enderror
-    <input type="submit" name="send" value="Send">
+                <input type="text" name="Nombre" id="" placeholder="Nombre" value="{{ old('Nombre') }}">
+                @error('Nombre')
+                    <h6>{{ $message }}</h6>
+                @enderror
 
-            </form> 
+                <input type="text" name="Apellido" id="" placeholder="Apellido" value="{{ old('Apellido') }}">
+                @error('Apellido')
+                    <h6>{{ $message }}</h6>
+                @enderror
+
+                <input type="email" name="email" id="" placeholder="Correo" value="{{ old('email') }}">
+                @error('email')
+                    <h6>{{ $message }}</h6>
+                @enderror
+                <input type="password" name="password" id="" placeholder="Contraseña" value="{{ old('password') }}">
+
+                @error('password')
+                    <h6>{{ $message }}</h6>
+                @enderror
+
+                <input type="text" name="Telefono" id="" placeholder="telefono" value="{{ old('Telefono') }}">
+                @error('Telefono')
+                    <h6>{{ $message }}</h6>
+                @enderror
+
+
+                <select name="Estado" id="Estado">
+                    <option value="Activo">Activo</option>
+                    <option value="Inactivo">Inactivo</option>
+                </select>
+                @error('Estado')
+                    <h6>{{ $message }}</h6>
+                @enderror
+
+                <select name="ID_rol" id="ID_rol">
+                    <option value="1">ADMINISTRADOR</option>
+                    <option value="2">CLIENTE</option>
+                    <option value="3">RECEPSIONISTA</option>
+                    <option value="4">MANTENIMIENTO SISTEMA</option>
+                    <option value="5">PERSONAL DE LIMPIEZA</option>
+                    <option value="6">SEGURIDAD</option>
+                </select>
+
+
+                @error('ID_rol')
+                    <h6>{{ $message }}</h6>
+                @enderror
+                <input class="crear"type="submit" name="send" value="Crear">
+
+            </form>
         </article>
     </section>
-   
+
 </body>
 
 
