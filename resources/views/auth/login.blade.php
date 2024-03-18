032152c077d947a32899119b74983159fab1afbe
ruben <rlozanotique@gmail.com>
2024-03-18 00:06:31 -0500

    estilos de factura

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,22 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}"> 
     <title>Hotel Lodging - Inicia Sesión</title>
 </head>
 <body>
+  
+  <a class="regresar" href="{{ asset('/') }}" >Regresar</a>
+  
 
-    <h1>Hotel Lodging <br>Inicia Sesión</h1>
+ 
+
+
+    
 
     <form action="{{ route('login') }}" method="POST" novalidate>
         @csrf
-
+        <h3>Hotel Lodging <br>Inicia Sesión</h3>
         @if (session('mensaje'))
             <h6>{{ session('mensaje') }}</h6>   
         @endif
 
         <div class="input-group">
-            <i class="fas fa-envelope"></i> <!-- Icono de correo electrónico -->
+            
+            <label for="Email">Email</label>
             <input type="email" name="email" placeholder="Correo electrónico" value="{{ old('email') }}">
         </div>
         @error('email')
@@ -27,7 +34,8 @@
         @enderror
 
         <div class="input-group">
-            <i class="fas fa-lock"></i> <!-- Icono de candado para la contraseña -->
+            
+            <label for="Password">Password</label>
             <input type="password" name="password" placeholder="Contraseña">
         </div>
         @error('password')
@@ -36,17 +44,14 @@
 
         <a href="{{ route('user.create') }}">crear cuenta</a>
 
-        <input  type="submit" name="login" value="Iniciar sesión">
-    </form>
+        <input class="boton"  type="submit" name="login" value="Iniciar sesión">
+    
 
-    <div class="social-login">
-        <p>O inicia sesión con:</p>
-        <div class="social-icons">
-            <a href="{{ url('auth/google') }}">
-                <img src="{{ asset('img/google.png') }}" alt="Google">
-            </a>
- 
-    </div>
+        <div class="social">
+            <div class="go"><i class="fab fa-google"></i>  Google</div>
+            <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+        </div>
+    </form>
 </div>
     
 </body>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,44 +1,47 @@
 <body>
+
+    <a class="regresar" href="{{ asset('/') }}" >Regresar</a>
    <form action="{{ route('user.store') }}" method="post">
    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
     @csrf
     
     @if (session('mensaje'))
-        <h6>{{ session('mensaje') }}</h6>   
+        <h6>{{ session('') }}</h6>   
     @endif
 
-    
+    <label for="Nro_doc">Numero de Documento</label>
     <input type="text" name="Nro_doc" id="" placeholder="Document Number" value="{{ old('Nro_doc') }}">
     @error('Nro_doc')
         <h6>{{ $message }}</h6>
     @enderror
-
+    <label for="Nombre">Nombre</label>
     <input type="text" name="Nombre" id="" placeholder="Nombre" value="{{ old('Nombre') }}">
     @error('Nombre')
         <h6>{{ $message }}</h6>
     @enderror
-
+    <label for="Apellido">apellido</label>
     <input type="text" name="Apellido" id="" placeholder="Apellido" value="{{ old('Apellido') }}">
     @error('Apellido')
         <h6>{{ $message }}</h6>
     @enderror
-  
+    <label for="Email">Email</label>
     <input type="email" name="email" id="" placeholder="Email" value="{{ old('email') }}">
     @error('email')
         <h6>{{ $message }}</h6>
     @enderror
+    <label for="Password">Password</label>
     <input type="password" name="password" id="" placeholder="Password" value="{{ old('password') }}">
 
     @error('password')
         <h6>{{ $message }}</h6>
     @enderror
-
+    <label for="Telefono">Telefono</label>
     <input type="text" name="Telefono" id="" placeholder="telefono" value="{{ old('Telefono') }}">
     @error('Telefono')
         <h6>{{ $message }}</h6>
     @enderror
 
-    
+    <label for="Estado">Estado</label>
     <select name="Estado" id="Estado">
         <option value="Activo">Activo</option>
         <option value="Inactivo">Inactivo</option>
@@ -46,7 +49,7 @@
     @error('Estado')
         <h6>{{ $message }}</h6>
     @enderror
-
+    <label for="Rol">Rol</label>
     <select name="ID_rol" id="ID_rol">
         <option value="1">ADMINISTRADOR</option>
         <option value="2">CLIENTE</option>
